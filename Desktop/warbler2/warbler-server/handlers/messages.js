445f7e26bb4f77eba27dabf3474b6ec8f63be36e
chriscebrero <chris.cebrero@gmail.com>
2018-06-23 15:33:57 -0700

    getmessage

--- Desktop/warbler2/warbler-server/handlers/messages.js
+++ Desktop/warbler2/warbler-server/handlers/messages.js
@@ -18,3 +18,16 @@ exports.createMessage = async function(req, res, next) {
     return next(err);
   }
 };
+
+exports.getMessage = async function(req, res, next) {
+  try {
+    let message = await db.Message.find(req.params.message_id);
+    return res.status(200).json(message);
+  } catch (err) {
+    return next(err)
+  }
+}
+
+exports.deleteMessage = async function(req, res, next) {
+
+}
