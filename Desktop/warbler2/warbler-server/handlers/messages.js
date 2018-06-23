0f5f096012c7a7842d4eb7b25cd6d40cc9b682ff
chriscebrero <chris.cebrero@gmail.com>
2018-06-23 15:41:17 -0700

    added backend

--- Desktop/warbler2/warbler-server/handlers/messages.js
+++ Desktop/warbler2/warbler-server/handlers/messages.js
@@ -29,5 +29,11 @@ exports.getMessage = async function(req, res, next) {
 }
 
 exports.deleteMessage = async function(req, res, next) {
-
-}
+  try {
+    let foundMessage = await db.Message.findById(req.params.message_id)
+    await foundMessage.remove()
+    return res.status(200).json(foundMessage);
+  } catch (err) {
+    return next(err);
+  }
+};
